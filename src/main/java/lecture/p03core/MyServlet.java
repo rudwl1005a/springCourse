e7202e77bd9c8640385d80c41ed9b8f60f42682f
Min kyungdae <rudwl1005@naver.com>
2021-12-29 05:10:13 +0900

    spring

---
+++ src/main/java/lecture/p03core/MyServlet.java
@@ -0,0 +1,23 @@
+package lecture.p03core;
+
+import lombok.Setter;
+
+@Setter
+public class MyServlet {
+	private MyDao dao = new MyDao();
+
+	public void doGet() {
+		// 0. 사전작업
+		// 필요한 객체 생성
+		
+		// 2. request handle
+		
+		
+		// 3. business logic
+		dao.update();
+		
+		// 4. add attribute
+		
+		// 5. forward / redirect
+	}
+}
